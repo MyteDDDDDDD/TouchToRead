1e5ed99c54bdb345a21a61de48ec89d41589dda6
MyteDDDDDDD <elvisyuan6988@gmail.com>
2017-06-22 17:55:49 +0800

    separation of text with contours method

--- TouchToRead/TouchToRead/mainFlow.cpp
+++ TouchToRead/TouchToRead/mainFlow.cpp
@@ -15,10 +15,15 @@ int main() {
 
 	Mat src = imread(srcURL);
 	imshow("orginal image", src);
+
+	Mat erodeIm;
+	Mat erodeEl = getStructuringElement(MORPH_RECT, Size(11, 11));
+	erode(src, erodeIm, erodeEl);
+	imshow("erode image", erodeIm);
+
 	//create a gray image
 	Mat grayIm(src.cols, src.rows, CV_8UC1);
-
-	creatGrayIm(src, grayIm);
+	creatGrayIm(erodeIm, grayIm);
 	//cvShowImage("title",grayIm);
 	
 	//create a binary image with the same size of gray image
@@ -27,14 +32,27 @@ int main() {
 	int threhold=127;
 
 	creatBinaryIm(grayIm, binIm,threhold);
-	//imshow("binary image", binIm);
+	imshow("binary image", binIm);
+
+	vector<vector<Point>> contours;
+	findContours(binIm, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+	
+	for (int i = 0; i < contours.size() - 1; i++) {
+		Rect rect = boundingRect(contours[i]);
+
+		if (rect.area() > 255) {
+			rectangle(src, rect, Scalar(255, 0, 0));
+		}
+		
+	}
+	
+	imshow("test", src);
 
 	//do projection
-	doProjection(binIm);
-	/*horizontalPro(binIm);
-	verticalPro(binIm);*/
+	/*doProjection(binIm);
 	showPro();
-	showRect(src);
+	showRect(src);*/
+	
 	cvWaitKey(0);
 
 
